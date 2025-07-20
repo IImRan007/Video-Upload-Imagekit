d8df9ad2d94821b046ba00b07d210bdc09f80b38
Mohd Imran <mohd78696321@gmail.com>
2025-07-20 19:53:46 +0530

    register and login

---
+++ app/login/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+function LoginPage() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (res?.error) {
+        console.error("Login failed:", res.error);
+      }
+
+      router.push("/");
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <div>
+        <p>
+          Don&apos;t have an account? <a href="/register">Register</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
