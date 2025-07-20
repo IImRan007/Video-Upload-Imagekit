b1656f04345c82906fb6c31143c98127ce300876
Mohd Imran <mohd78696321@gmail.com>
2025-07-20 19:28:54 +0530

    image kit auth

--- models/Video.ts
+++ models/Video.ts
@@ -21,18 +21,23 @@ export interface IVideo {
   updatedAt?: Date;
 }
 
-const videoSchema = new Schema<IVideo>({
-  title: { type: String, required: true, trim: true },
-  description: { type: String, required: true, trim: true },
-  videoUrl: { type: String, required: true },
-  thumbnailUrl: { type: String, required: true },
-  controls: { type: Boolean, default: true },
-  transformation: {
-    width: { type: Number, default: VIDEO_DIMENSIONS.width },
-    height: { type: Number, default: VIDEO_DIMENSIONS.height },
-    quality: { type: Number, min: 1, max: 100 },
+const videoSchema = new Schema<IVideo>(
+  {
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    videoUrl: { type: String, required: true },
+    thumbnailUrl: { type: String, required: true },
+    controls: { type: Boolean, default: true },
+    transformation: {
+      width: { type: Number, default: VIDEO_DIMENSIONS.width },
+      height: { type: Number, default: VIDEO_DIMENSIONS.height },
+      quality: { type: Number, min: 1, max: 100 },
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Video = models?.Video || model<IVideo>("Video", videoSchema);
 
